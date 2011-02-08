952b0f35ecd3a1195affa380352c9672416c8bee
citrus <spencer@citrusme.com>
2011-02-08 02:23:49 -0800

    added anchor links to states

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,10 +1,7 @@
 class RetailersController < Spree::BaseController
 
   def index
-    @retailers = Retailer.order(:name) 
-    #if request.xhr? 
-    #  render :partial => 'list', :layout => false, :locals => { :retailers => @retailers }      
-    #end
+    @retailers = Retailer.order(:state, :name)
   end
 
 end
