8e7fd92020d9587ac64f548f7e20c05120863121
Spencer Steffen <spencer@citrusme.com>
2012-02-29 10:34:38 -0800

    rewrite before_migrate hook for 1.0.0 and include user error fix

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -1,8 +1,18 @@
-# run install generators
-run "rails g spree:site"
+# Install spree core and migrate db
+rake "spree:install:migrations"
+
+# Mount the spree core engine routes
+insert_into_file File.join('config', 'routes.rb'), :after => "Application.routes.draw do\n" do
+  "  # Mount Spree's routes\n  mount Spree::Core::Engine, :at => '/'\n"
+end
+
+# Fix uninitialized constant Spree::User::DestroyWithOrdersError 
+template "spree_user_error_fix.rb", "config/initializers/spree_user_error_fix.rb"
+
+# Install spree_retailers
 run "rails g spree_retailers:install"
 
-# remove all stylesheets except core  
+# Remove all stylesheets except core  
 %w(admin store).each do |ns|
   template "#{ns}/all.js", "app/assets/javascripts/#{ns}/all.js", :force => true
   template "#{ns}/all.css", "app/assets/stylesheets/#{ns}/all.css", :force => true
